2a0793e7157e3c7bdcb2de3504d191d50b3e3391
ofbennett <ofbennett@gmail.com>
2015-11-24 10:19:58 +0000

    name == main line added to class file

--- greengrapher/greengraphpak.py
+++ greengrapher/greengraphpak.py
@@ -64,7 +64,8 @@ class Map(object):
         result = img.imsave(buffer, out, format='png')
         return buffer.getvalue()
 
-mygraph=Greengraph('New York','Chicago')
-data = mygraph.green_between(20)
-plt.plot(data)
-plt.show()
+if __name__ == '__main__':
+    mygraph=Greengraph('New York','Chicago')
+    data = mygraph.green_between(20)
+    plt.plot(data)
+    plt.show()
