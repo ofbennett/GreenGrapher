ab1a369ab48682e0d86f2635c714151a36315930
ofbennett <ofbennett@gmail.com>
2015-11-24 17:49:36 +0000

    fixed problem with setup file and changed the argument passing method

--- greengrapher/command.py
+++ greengrapher/command.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = 'A program which displays a graph of the amount of green along a straight line between two cities on Google Maps')
-    parser.add_argument('city1')
-    parser.add_argument('city2')
+    parser.add_argument('--begin','-b',default = 'London', help = 'City to start measuring from')
+    parser.add_argument('--end','-e',default = 'Oxford', help = 'City to finish measuring at')
     parser.add_argument('--steps','-s',help = 'Number of equal steps between cities to show greenness. Default is 20.',default = 20)
     arguments = parser.parse_args()
-    # print arguments.city1, arguments.city2, arguments.steps
-    mygraph = Greengraph(arguments.city1,arguments.city2)
+    #print arguments.begin, arguments.end, arguments.steps
+    mygraph = Greengraph(arguments.begin,arguments.end)
     data = mygraph.green_between(arguments.steps)
     plt.plot(data)
     plt.show()
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = 'greengrapher',
     version = '1.0',
     packages = find_packages(exclude = ['*test']),
-    scripts = ['scripts/greengraph'],
-    install_requires = ['numpy','matplotlib','geopy','requests','StringIO','argparse','nose']
+    scripts = ['scripts/greengraph.py'],
+#    install_requires = ['numpy','matplotlib','geopy','requests','StringIO','argparse','nose']
+    install_requires = ['numpy','matplotlib','geopy','requests','nose']
 )
