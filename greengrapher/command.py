19c872589f8937291f9976717da0d9e6bf56b871
ofbennett <ofbennett@gmail.com>
2015-11-24 22:12:09 +0000

    out argument added to command file

--- greengrapher/command.py
+++ greengrapher/command.py
@@ -7,6 +7,7 @@ def process():
     parser.add_argument('--begin','-b',default = 'London', help = 'City to start measuring from')
     parser.add_argument('--end','-e',default = 'Oxford', help = 'City to finish measuring at')
     parser.add_argument('--steps','-s',help = 'Number of equal steps between cities to show greenness. Default is 20.',default = 20)
+    parser.add_argument('--out','-o',default = 'graph.png',help = 'Name of output file to save graph to')
     arguments = parser.parse_args()
     #print arguments.begin, arguments.end, arguments.steps
     mygraph = Greengraph(arguments.begin,arguments.end)
