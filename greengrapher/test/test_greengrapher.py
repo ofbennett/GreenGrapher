c5ae9de63e4017399cdcca412df2a8a09463d7b4
ofbennett <ofbennett@gmail.com>
2015-11-29 20:59:07 +0000

    example test added

--- greengrapher/test/test_greengrapher.py
+++ greengrapher/test/test_greengrapher.py
@@ -1,5 +1,5 @@
 from nose.tools import assert_raises, assert_equal
 from .. import command,greengraphertools
 
-def test_command():
+def test_example():
     assert_equal(1,1,msg='Here is my great message')
