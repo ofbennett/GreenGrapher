c3fede277001d6f498b2b3204e4dc1e3d7244396
ofbennett <ofbennett@gmail.com>
2015-11-29 17:38:46 +0000

    Added save figure action to command file

--- greengrapher/command.py
+++ greengrapher/command.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 def process():
     parser = ArgumentParser(description = 'A program which displays a graph of the amount of green along a straight line between two cities on Google Maps')
-    parser.add_argument('--begin','-b',default = 'London', help = 'City to start measuring from')
+    parser.add_argument('--begin','-b',default = 'Cambridge', help = 'City to start measuring from')
     parser.add_argument('--end','-e',default = 'Oxford', help = 'City to finish measuring at')
     parser.add_argument('--steps','-s',help = 'Number of equal steps between cities to show greenness. Default is 20.',default = 20)
     parser.add_argument('--out','-o',default = 'graph.png',help = 'Name of output file to save graph to')
@@ -13,6 +13,7 @@ def process():
     mygraph = Greengraph(arguments.begin,arguments.end)
     data = mygraph.green_between(arguments.steps)
     plt.plot(data)
+    plt.savefig(arguments.out)
     plt.show()
 
 if __name__ == '__main__':
