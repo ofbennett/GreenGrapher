29feb8b4c66bf83b9fcf6d707aba0e1583cdcb19
ofbennett <ofbennett@gmail.com>
2015-11-30 17:27:02 +0000

    refactored tests

--- greengrapher/test/test_greengrapher.py
+++ greengrapher/test/test_greengrapher.py
@@ -13,6 +13,7 @@ def test_construct_Map():
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
+
         mock_get.assert_called_with(
         "http://maps.googleapis.com/maps/api/staticmap?",
         params = {
@@ -26,82 +27,94 @@ def test_construct_Map():
 
 def test_green_when_map_not_green():
     #Tests the Map.green() method when the map has no green pixels
+    not_green_array = np.ones([5,5,3])
+
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            not_green_array = np.ones([5,5,3])
             mock_map.pixels = not_green_array
             green_pixels = mock_map.green(1.1)
-            all_false = np.zeros([5,5])
+            all_false = np.zeros(np.shape(green_pixels))
+
             outcome = np.array_equal(green_pixels,all_false)
             assert_equal(outcome,True)
 
 def test_green_when_map_all_green():
     #Tests the Map.green() method when the map has all green pixels
+    green_array = np.zeros([5,5,3])
+    green_array[:,:,1] = 1
+
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            green_array = np.zeros([5,5,3])
-            green_array[:,:,1] = 1
             mock_map.pixels = green_array
             green_pixels = mock_map.green(1.1)
-            all_true = np.ones([5,5])
+            all_true = np.ones(np.shape(green_pixels))
+
             outcome = np.array_equal(green_pixels,all_true)
             assert_equal(outcome,True)
 
 def test_count_green_when_map_not_green():
     #Tests the Map.count_green() method when the map has no green pixels
+    not_green_array = np.ones([5,5,3])
+
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            not_green_array = np.ones([5,5,3])
             mock_map.pixels = not_green_array
             green_pixel_count = mock_map.count_green(1.1)
+
             assert_equal(green_pixel_count,0)
 
 def test_count_green_when_map_all_green():
     #Tests the Map.count_green() method when the map has all green pixels
+    green_array = np.zeros([5,5,3])
+    green_array[:,:,1] = 1
+
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            green_array = np.zeros([5,5,3])
-            green_array[:,:,1] = 1
             mock_map.pixels = green_array
             green_pixel_count = mock_map.count_green(1.1)
-            assert_equal(green_pixel_count,25)
+
+            assert_equal(green_pixel_count,np.size(green_array)/3)
 
 def test_show_green_when_map_not_green():
     #Tests the Map.show_green() method when the map has no green pixels
+    not_green_array = np.ones([5,5,3])
+
     from StringIO import StringIO
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            not_green_array = np.ones([5,5,3])
             mock_map.pixels = not_green_array
             with patch.object(img,'imsave') as mock_imsave:
                 mock_map.show_green()
-                test_array = np.zeros([5,5,3])
+                test_array = np.zeros(np.shape(not_green_array))
                 calls = mock_imsave.call_args
                 arg,kwrds = calls
+
                 assert_equal({'format':'png'},kwrds)
                 outcome = (np.array_equal(test_array, arg[0]) or np.array_equal(test_array, arg[1]))
                 assert_equal(outcome,True)
 
 def test_show_green_when_map_all_green():
     #Tests the Map.show_green() method when the map has all green pixels
+    green_array = np.zeros([5,5,3])
+    green_array[:,:,1] = 1
+
     from StringIO import StringIO
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
             mock_map = greengraphertools.Map(111,222)
-            green_array = np.zeros([5,5,3])
-            green_array[:,:,1] = 1
             mock_map.pixels = green_array
             with patch.object(img,'imsave') as mock_imsave:
                 mock_map.show_green()
-                test_array = np.zeros([5,5,3])
+                test_array = np.zeros(np.shape(green_array))
                 test_array[:,:,1] = 1
                 calls = mock_imsave.call_args
                 arg,kwrds = calls
+
                 assert_equal({'format':'png'},kwrds)
                 outcome = (np.array_equal(test_array, arg[0]) or np.array_equal(test_array, arg[1]))
                 assert_equal(outcome,True)
@@ -109,6 +122,7 @@ def test_show_green_when_map_all_green():
 def test_construct_Greengraph():
     #Tests the Greengraph class constructor
     graph = greengraphertools.Greengraph('London', 'Oxford')
+
     assert_equal(['London','Oxford'],[graph.start,graph.end])
 
 def test_geolocate():
@@ -117,6 +131,7 @@ def test_geolocate():
     with patch.object(graph.geocoder,'geocode') as mock_geocode:
         mock_geocode.return_value = [[0,(51.5073509, -0.1277583)],[0,0]]
         coordinates = graph.geolocate('London')
+
         mock_geocode.assert_called_with('London',exactly_one=False)
         assert_equal(coordinates,(51.5073509, -0.1277583))
 
@@ -126,8 +141,10 @@ def test_location_sequence():
     end = [10,10]
     steps = 10
     expected = [[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[8,8],[9,9],[10,10]]
+
     graph = greengraphertools.Greengraph('London', 'Oxford')
     result = graph.location_sequence(start,end,steps)
+
     outcome = np.array_equal(result,expected)
     assert_equal(outcome,True)
 
@@ -135,6 +152,7 @@ def test_green_between():
     #Test the Greengraph.green_between method with mocks
     steps = 10
     green_counts = [1,1,2,4,3,4,5,3,4,7]
+
     graph = greengraphertools.Greengraph('London', 'Oxford')
     with patch.object(requests,'get') as mock_get:
         with patch.object(img,'imread') as mock_imread:
@@ -144,6 +162,7 @@ def test_green_between():
                                                   (51.7520209, -1.2577263)]
                     mock_count_green.side_effect = green_counts
                     result = graph.green_between(steps)
+
                     assert_equal(result,green_counts)
 
 def test_process():
@@ -164,7 +183,7 @@ def test_process():
             self.out = out
 
     expected = Expected(begin,end,steps,out)
-    
+
     with patch.object(ArgumentParser,'parse_args') as mock_parse_args:
         mock_parse_args.return_value = expected
         with patch.object(greengraphertools.Greengraph,'green_between') as mock_green_between:
@@ -173,6 +192,7 @@ def test_process():
                 with patch.object(plt,'show') as mock_show:
                     with patch.object(plt,'plot') as mock_plot:
                         command.process()
+                        
                         mock_parse_args.assert_called_once
                         mock_green_between.assert_called_with(expected.steps)
                         mock_savefig.assert_called_with(expected.out)
